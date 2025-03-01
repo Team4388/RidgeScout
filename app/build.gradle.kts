0846f5a3b3198f349a47e6a2215b2f410da5925d
Michael Mikovsky <77305074+Astatin3@users.noreply.github.com>
2025-03-01 00:06:53 -0700

    Increment version number again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.ridgebotics.ridgescout"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8    // **IMPORTANT** Increment this before releasing on github
-        versionName = "1.1"// **IMPORTANT** Change this before releasing on github (<Year num since 2024>.<Update Version>)
+        versionCode = 9    // **IMPORTANT** Increment this before releasing on github
+        versionName = "1.2"// **IMPORTANT** Change this before releasing on github (<Year num since 2024>.<Update Version>)
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
